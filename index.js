1526879c08352360af004646a9a38798e571603e
Dan Piessens <dan.piessens@tcreveal.com>
2018-10-03 11:42:14 -0500

    Updated code to post to log analytics

--- index.js
+++ index.js
@@ -79,65 +79,18 @@ function lastLogCheckpoint (req, res) {
     const exportLogs = (logs, callback) => {
       console.log('Exporting logs to Azure Log Analytics: ' + logs.length);
 
-      logs.forEach(function(record) {
-        var level = 0;
-        record.type_code = record.type;
-        if (logTypes[record.type]) {
-          level = logTypes[record.type].level;
-          record.type = logTypes[record.type].event;
-        }
-
-        // Application Insights does not like null or empty strings.
-        if (!record.ip || record.ip === '') delete record.ip;
-        if (!record.user_id || record.user_id === '')  delete record.user_id;
-        if (!record.user_name || record.user_name === '')  delete record.user_name;
-        if (!record.connection || record.connection === '')  delete record.connection;
-        if (!record.client_name || record.client_name === '') delete record.client_name;
-        if (!record.description || record.description === '')  delete record.description;
-
-        // Application Insights does not like booleans.
-        record.isMobile = record.isMobile && 'yes' || 'no';
-
-        // Application Insights does not like objects.
-        if (record.details) {
-          record.details = JSON.stringify(record.details, null, 2);
-        }
-
-        // Application Insights does not like login strings.
-        if (record.details && record.details.length > 8185) {
-          record.details = record.details.substring(0, 8185) + '...';
-        }
-
-        var agent = useragent.parse(record.user_agent);
-        record.os = agent.os.toString();
-        record.os_version = agent.os.toVersion();
-        record.device = agent.device.toString();
-        record.device_version = agent.device.toVersion();
-
-        // Don't show "Generic Smartphone" in Application Insightis.
-        if (record.device && record.device.indexOf('Generic Smartphone') >= 0) {
-          record.device = agent.os.toString();
-          record.device_version = agent.os.toVersion();
-        }
-
-        if (level >= 4) {
-          var error = new Error(record.type);
-          error.name = record.type;
-          client.trackException(error, record);
-        }
-
-        client.trackEvent(record.type, record);
-      });
-
       if (logs && logs.length) {
-        console.log('Flushing all data...');
+        console.log('Sending all data...');
 
-        client.sendPendingData((response) => {
-          return callback(null, response);
-        });
+        postLogsFromToLogAnalytics(
+          ctx.data.LOG_ANALYTICS_WORKSPACE_ID,
+          ctx.data.LOG_ANALYTICS_SHARED_KEY,
+          logs,
+          (response) => {
+            return callback(null, response);
+          });
       } else {
-        console.log('No data to flush...');
-
+        console.log('No data to send...');
         return callback(null, '{ "itemsAccepted": 0 }');
       }
     };
@@ -198,199 +151,6 @@ function lastLogCheckpoint (req, res) {
   });
 }
 
-const logTypes = {
-  's': {
-    event: 'Success Login',
-    level: 1 // Info
-  },
-  'seacft': {
-    event: 'Success Exchange',
-    level: 1 // Info
-  },
-  'seccft': {
-    event: 'Success Exchange (Client Credentials)',
-    level: 1 // Info
-  },
-  'feacft': {
-    event: 'Failed Exchange',
-    level: 3 // Error
-  },
-  'feccft': {
-    event: 'Failed Exchange (Client Credentials)',
-    level: 3 // Error
-  },
-  'f': {
-    event: 'Failed Login',
-    level: 3 // Error
-  },
-  'w': {
-    event: 'Warnings During Login',
-    level: 2 // Warning
-  },
-  'du': {
-    event: 'Deleted User',
-    level: 1 // Info
-  },
-  'fu': {
-    event: 'Failed Login (invalid email/username)',
-    level: 3 // Error
-  },
-  'fp': {
-    event: 'Failed Login (wrong password)',
-    level: 3 // Error
-  },
-  'fc': {
-    event: 'Failed by Connector',
-    level: 3 // Error
-  },
-  'fco': {
-    event: 'Failed by CORS',
-    level: 3 // Error
-  },
-  'con': {
-    event: 'Connector Online',
-    level: 1 // Info
-  },
-  'coff': {
-    event: 'Connector Offline',
-    level: 3 // Error
-  },
-  'fcpro': {
-    event: 'Failed Connector Provisioning',
-    level: 4 // Critical
-  },
-  'ss': {
-    event: 'Success Signup',
-    level: 1 // Info
-  },
-  'fs': {
-    event: 'Failed Signup',
-    level: 3 // Error
-  },
-  'cs': {
-    event: 'Code Sent',
-    level: 0 // Debug
-  },
-  'cls': {
-    event: 'Code/Link Sent',
-    level: 0 // Debug
-  },
-  'sv': {
-    event: 'Success Verification Email',
-    level: 0 // Debug
-  },
-  'fv': {
-    event: 'Failed Verification Email',
-    level: 0 // Debug
-  },
-  'scp': {
-    event: 'Success Change Password',
-    level: 1 // Info
-  },
-  'fcp': {
-    event: 'Failed Change Password',
-    level: 3 // Error
-  },
-  'sce': {
-    event: 'Success Change Email',
-    level: 1 // Info
-  },
-  'fce': {
-    event: 'Failed Change Email',
-    level: 3 // Error
-  },
-  'scu': {
-    event: 'Success Change Username',
-    level: 1 // Info
-  },
-  'fcu': {
-    event: 'Failed Change Username',
-    level: 3 // Error
-  },
-  'scpn': {
-    event: 'Success Change Phone Number',
-    level: 1 // Info
-  },
-  'fcpn': {
-    event: 'Failed Change Phone Number',
-    level: 3 // Error
-  },
-  'svr': {
-    event: 'Success Verification Email Request',
-    level: 0 // Debug
-  },
-  'fvr': {
-    event: 'Failed Verification Email Request',
-    level: 3 // Error
-  },
-  'scpr': {
-    event: 'Success Change Password Request',
-    level: 0 // Debug
-  },
-  'fcpr': {
-    event: 'Failed Change Password Request',
-    level: 3 // Error
-  },
-  'fn': {
-    event: 'Failed Sending Notification',
-    level: 3 // Error
-  },
-  'sapi': {
-    event: 'API Operation'
-  },
-  'fapi': {
-    event: 'Failed API Operation'
-  },
-  'limit_wc': {
-    event: 'Blocked Account',
-    level: 4 // Critical
-  },
-  'limit_ui': {
-    event: 'Too Many Calls to /userinfo',
-    level: 4 // Critical
-  },
-  'api_limit': {
-    event: 'Rate Limit On API',
-    level: 4 // Critical
-  },
-  'sdu': {
-    event: 'Successful User Deletion',
-    level: 1 // Info
-  },
-  'fdu': {
-    event: 'Failed User Deletion',
-    level: 3 // Error
-  },
-  'fapi': {
-    event: 'Failed API Operation',
-    level: 3 // Error
-  },
-  'limit_wc': {
-    event: 'Blocked Account',
-    level: 3 // Error
-  },
-  'limit_mu': {
-    event: 'Blocked IP Address',
-    level: 3 // Error
-  },
-  'slo': {
-    event: 'Success Logout',
-    level: 1 // Info
-  },
-  'flo': {
-    event: ' Failed Logout',
-    level: 3 // Error
-  },
-  'sd': {
-    event: 'Success Delegation',
-    level: 1 // Info
-  },
-  'fd': {
-    event: 'Failed Delegation',
-    level: 3 // Error
-  }
-};
-
 function getLogsFromAuth0 (domain, token, take, from, cb) {
   var url = `https://${domain}/api/v2/logs`;
 
@@ -418,27 +178,31 @@ function getLogsFromAuth0 (domain, token, take, from, cb) {
   });
 }
 
-function postLogsFromToLogAnalytics (domain, token, take, from, cb) {
-  var url = `https://${domain}/api/v2/logs`;
+function postLogsFromToLogAnalytics (workspaceId, sharedKey, jsonData, cb) {
+  var apiVersion = '2016-04-01';
+  var processingDate = new Date().toUTCString();
+  
+  var body = JSON.stringify(jsonData);    
+  var contentLength = Buffer.byteLength(body, 'utf8');
 
-  Request({
-    method: 'GET',
-    url: url,
-    json: true,
-    qs: {
-      take: take,
-      from: from,
-      sort: 'date:1',
-      per_page: take
-    },
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: 'application/json'
-    }
-  }, (err, res, body) => {
-    if (err) {
-      console.log('Error getting logs', err);
-      cb(null, err);
+  var stringToSign = 'POST\n' + contentLength + '\napplication/json\nx-ms-date:' + processingDate + '\n/api/logs';
+  var signature = crypto.createHmac('sha256', new Buffer(sharedKey, 'base64')).update(stringToSign, 'utf-8').digest('base64');
+  var authorization = 'SharedKey ' + workspaceId + ':' + signature;
+
+  var headers = {
+      'content-type': 'application/json', 
+      'Authorization': authorization,
+      'Log-Type': 'Auth0Logs',
+      'x-ms-date': processingDate,
+      'time-generated-field': 'date'
+  };
+
+  var url = 'https://' + workspaceId + '.ods.opinsights.azure.com/api/logs?api-version=' + apiVersion;
+
+  Request.post({url: url, headers: headers, body: body}, function (error, response, body) {
+    if (error) {
+      console.log('Error sending logs to Azure Log Analytics', error);
+      cb(null, error);
     } else {
       cb(body);
     }
